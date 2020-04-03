4605ec8fb1d52f8b4d180655a2d72e165a273f0a
Robear <robert.roussely.lp4@gmail.com>
2020-04-03 14:09:38 +0200

    Ajout footer

--- src/app/material/material.module.ts
+++ src/app/material/material.module.ts
@@ -1,8 +1,10 @@
 import { NgModule } from '@angular/core';
-import { MatButtonModule} from '@angular/material';
+import { MatButtonModule, MatToolbarModule, MatIconModule} from '@angular/material';
 
 const MaterialComponents =[
-  MatButtonModule
+  MatButtonModule,
+  MatToolbarModule,
+  MatIconModule
 ]
 
 @NgModule({
